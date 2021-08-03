643c2e9294a009e6f2e2c20085d331ce6e326c3d
MerleStr <merle-struckmann5.8@t-online.de>
2021-08-03 15:34:54 +0200

    Load Data

--- Projekt/visualisierung-projekt/src/app/components/graph-panel/graph-panel.component.ts
+++ Projekt/visualisierung-projekt/src/app/components/graph-panel/graph-panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import { svg } from 'd3';
 
 @Component({
   selector: 'app-graph-panel',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  createGraphSvg() {
+      d3.csv('./Data/Output/Koeln.csv').then(function(data){
+
+      })
+  }
 }
