4a1e6ac9d155a1a5df7af5daa8cff5975cdf2a19
MerleStr <merle-struckmann5.8@t-online.de>
2021-09-05 19:35:30 +0200

    Progress

--- Projekt/visualisierung-projekt/src/app/components/map-graph/map-graph.component.ts
+++ Projekt/visualisierung-projekt/src/app/components/map-graph/map-graph.component.ts
@@ -38,6 +38,7 @@ export class MapGraphComponent implements OnInit {
   ngOnInit(): void {
     this.addMap(this.width, this.height);
     this.addMinimap(this.width, this.widthDe, this.heightDe)
+    this.addLegend("heatmap", d3.interpolatePlasma);
   }
 
   selectCity(selectedCity: string) {
@@ -252,4 +253,33 @@ export class MapGraphComponent implements OnInit {
       })
     })
   }
+
+  private addLegend(id: any, interpolator: any) {
+    var data = Array.from(Array(100).keys());
+
+    var cScale = d3.scaleSequential()
+        .interpolator(interpolator)
+        .domain([0,99]);
+
+    var xScale = d3.scaleLinear()
+        .domain([0,99])
+        .range([0, 580]);
+
+    var u = d3.select("#" + id)
+        .selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", (d) => Math.floor(xScale(d)))
+        .attr("y", 0)
+        .attr("height", 40)
+        .attr("width", (d) => {
+            if (d == 99) {
+                return 6;
+            }
+            return Math.floor(xScale(d+1)) - Math.floor(xScale(d)) + 1;
+         })
+        .attr("fill", (d) => cScale(d));
+  }
+
 }
--- Projekt/visualisierung-projekt/src/app/components/panel-container/panel-container.component.ts
+++ Projekt/visualisierung-projekt/src/app/components/panel-container/panel-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 import { DataClientService } from 'src/app/services/data-client.service';
 import { DataService } from 'src/app/services/data.service';
 
@@ -22,9 +23,35 @@ export class PanelContainerComponent implements OnInit {
   private CityAddedHandle() {
     if(this.dataService.displayedCities.size === 0) {
       this.toggleView(false)
+      d3.selectAll(".panel-filter-button").style("background-color", "#969696");
+      var x = document.getElementsByClassName("panel-filter-button");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          (<HTMLElement>x[i]).removeAttribute("enabled");
+          (<HTMLElement>x[i]).setAttribute("disabled", "disabled");
+        }
+        var x = document.getElementsByClassName("panel-control-button");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          (<HTMLElement>x[i]).removeAttribute("enabled");
+          (<HTMLElement>x[i]).setAttribute("disabled", "disabled");
+        }
     }
     else {
       this.toggleView(true)
+      d3.selectAll(".panel-filter-button").style("background-color", "#f5f5f5");
+      var x = document.getElementsByClassName("panel-filter-button");
+      var i;
+      for (i = 0; i < x.length; i++) {
+        (<HTMLElement>x[i]).removeAttribute("disabled");
+        (<HTMLElement>x[i]).setAttribute("enabled", "enabled");
+      }
+      var x = document.getElementsByClassName("panel-control-button");
+      var i;
+      for (i = 0; i < x.length; i++) {
+        (<HTMLElement>x[i]).removeAttribute("disabled");
+        (<HTMLElement>x[i]).setAttribute("enabled", "enabled");
+      }
     }
   }
 
